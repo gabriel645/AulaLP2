c160d3a90c1fc3c5693dda028b02d1efe365a31c
082160007 <082160007@HPFS-CE-LAB6913.cefsa.corp.local>
2019-02-06 22:23:05 -0200

    First code

---
+++ src/Projeto1.java
@@ -0,0 +1,42 @@
+
+public class Projeto1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		
+		Calc c = new Calc();
+		c.setX(10);
+		c.setY(1);
+		
+		System.out.println(c.add());
+		
+	}
+
+
+}
+
+class Calc{
+	int x,y;
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+	
+	public int add(){
+		return this.getX() + this.getY();
+	}
+}
+
